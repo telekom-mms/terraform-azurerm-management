e85be6a2813aba723da5b948f2b8c278f0aba0e7
mima <Michaela.Mattes@t-systems.com>
2021-12-16 20:26:51 +0100

    fix: variable name

--- variables.tf
+++ variables.tf
@@ -14,7 +14,7 @@ locals {
   }
 
   # deep merge
-  keyvault_config = {
+  management_lock_config = {
     for config in keys(local.default.management_lock_config) :
     config => {
       for instance in keys(var.management_lock_config[config]) :
